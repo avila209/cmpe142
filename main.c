fcbc0b019c1720da2722df595407f421d7d014f1
Luis <luis.avila@sjsu.edu>
2019-03-11 22:40:27 -0700

    Version 1 foo's

--- main.c
+++ main.c
@@ -14,30 +14,29 @@ char **parser(char *line);
 int main()
 {
     char *line = NULL;
-    char *cmd = NULL;
-    char *argument = NULL;
     char **sep;
     size_t linesize = 0;
     ssize_t linelen;
     pid_t pid;
     int stat_loc;
 
+
     while ((linelen = getline(&line, &linesize, stdin) != -1)) {
-        printf("yeesh>");
+        //printf("yeesh>");
         if(strncmp("exit", line, 4) == 0){
             exit(0);
         }
 
         sep = parser(line);
-
         pid = fork();
-            if(pid == 0) {
-                execvp(sep[0], sep);
-                printf("Execution failed \n");
-            }
-            else{
-                waitpid(pid, &stat_loc, WUNTRACED);
-            }
+        if (pid == 0) {
+            execvp(sep[0], sep);
+            printf("Execution failed \n");
+        }
+        else {
+            waitpid(pid, &stat_loc, WUNTRACED);
+        }
+
     }
 
     free(line);
@@ -47,6 +46,7 @@ int main()
 
 
 char **parser(char *line) {
+    line = strtok(line, "\n");
     char *parsed;
     char **sep = malloc(8*sizeof(char *));
     int index = 0;
